34480f75c32f548902d28491a2947b8b011b9f78
sonrichard <134939808+sonrichard@users.noreply.github.com>
2023-12-26 21:02:40 -0300

    isso

---
+++ data.js
@@ -0,0 +1,44 @@
+let songs = [
+    {
+        name: 'Akari(Dalila)',
+        path: 'song/1.wav',
+        artist: 'Dalila Antonio',
+        cover: 'images/f1.jpg'
+
+    },
+    {
+        name: 'Menya Marcelino',
+        path: 'song/2.wav',
+        artist: 'Esmenia Marcelino',
+        cover: 'images/f2.jpg'
+
+    },
+    {
+        name: 'Kira(Clopass)',
+        path: 'song/3.wav',
+        artist: 'Clopass De Souza',
+        cover: 'images/f3.jpg'
+
+    },
+    {
+        name: 'Chara',
+        path: 'song/4.wav',
+        artist: 'Carlos Isaque',
+        cover: 'images/f4.jpg'
+
+    },
+    {
+        name: 'Leitinho',
+        path: 'song/5.wav',
+        artist: 'Antonio Manecas',
+        cover: 'images/f5.jpg'
+
+    },
+    {
+        name: 'Akira(Ryome)',
+        path: 'song/6.wav',
+        artist: 'Richard Moises(Criador)',
+        cover: 'images/f6.jpg'
+
+    }
+]
